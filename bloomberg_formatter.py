60bea6cfdd7a4707ce5489ea24ad59e2c3877d03
shamsad-h <101060225+shamsad-h@users.noreply.github.com>
2022-03-14 12:04:48 +0000

    Update bloomberg_formatter.py

--- bloomberg_formatter.py
+++ bloomberg_formatter.py
@@ -5,7 +5,7 @@ with open('bloomberg_article.txt', 'r', encoding='utf-8') as f:
     
 abbrvs = ['Inc.', 'E.g.', 'e.g.', 'Ltd.']
 
-out = re.sub('(?<![."”?])\s*\n', ' ', out)
+out = re.sub('(?<![."”?!])\s*\n', ' ', out)
 out = out.replace("(Bloomberg) -- ", "\n")
 for a in abbrvs:
     out = out.replace(a+'\n', a+' ')
