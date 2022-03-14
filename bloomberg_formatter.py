5aa08d5622bf2ae94d50936f567d34633a2236a5
shamsad-h <101060225+shamsad-h@users.noreply.github.com>
2022-03-14 10:43:18 +0000

    Initial commit

---
+++ bloomberg_formatter.py
@@ -0,0 +1,14 @@
+import re
+
+with open('bloomberg_article.txt', 'r', encoding='utf-8') as f:
+    out = f.read()
+    
+abbrvs = ['Inc.', 'E.g.', 'e.g.', 'Ltd.']
+
+out = re.sub('(?<![."”?])\s*\n', ' ', out)
+out = out.replace("(Bloomberg) -- ", "\n")
+for a in abbrvs:
+    out = out.replace(a+'\n', a+' ')
+out = out.replace('\n', '\n\n')
+with open('bloomberg_article_formatted.txt', 'w', encoding='utf-8') as f:
+    f.write(out)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Bloomberg article formatter
+
+This is a script written in Python that uses regular expressions to automatically format articles sent from the Bloomberg terminal.
+
+The script runs on a file called "bloomberg_article.txt" and outputs the formatted file as "bloomberg_article_formatted.txt"
+
+The "bloomberg_article.txt" can contain as many articles as required.
+
+Some light formatting may be required at the end.
